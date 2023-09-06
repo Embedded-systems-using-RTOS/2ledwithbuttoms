1d021adfc89da86ea64cfca6ce6913237c4afd94
peterawad <96942247+peterawad@users.noreply.github.com>
2023-09-06 18:18:06 +0100

    Update LEDbuttom.c

--- 2LEDbuttom.c
+++ 2LEDbuttom.c
@@ -9,20 +9,25 @@
 
 void app_main(void)
 {
+    // Configure the GPIO pins for the LED and push button
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
 
     while(1) 
-    {       
+    {
+        // Check the state of the push button
         if (gpio_get_level(PUSH_BUTTON_PIN) == 1)
         {  
+            // If the push button is pressed (high state), turn on the LED
             gpio_set_level(LED_PIN, 1);        
         } 
         else
         {
+            // If the push button is not pressed (low state), turn off the LED
             gpio_set_level(LED_PIN, 0);        
         }
 
+        // Introduce a small delay to prevent rapid toggling
         vTaskDelay(1);
     }
 }
